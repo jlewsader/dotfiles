139066459d32be36629c4dc70cc1221def12a15e
jlewsader <jlewsader@gmail.com>
2025-12-26 10:08:14 -0500

    Update p10k config to remove the user/host name from prompt

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -915,7 +915,7 @@
   # Context format when running with privileges: user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%n'
   # Context format when in SSH without privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%m'
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE=''
   # Default context format (no privileges, no SSH): user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE=''
 
