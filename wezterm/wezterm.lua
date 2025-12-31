ade00108ac10ce0d8d9b248778799327d5f63523
Justin Lewsader <jlewsader@gmail.com>
2025-12-31 12:32:16 -0500

    Add opencode CLI to zshrc, fix wezterm window sizing on laptop

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,5 @@
 local wezterm = require("wezterm")
 
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
-end)
-
 return {
 	color_scheme = "Dracula",
 	font_size = 12.0,
