a83eda06f2d5f9ece70f50f907d450ff126180a8
Justin Lewsader <jlewsader@gmail.com>
2026-01-06 10:42:39 -0500

    Remove Hyprland, Waybar, and Wezterm - no longer using

--- wezterm/wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require("wezterm")
-
-return {
-	color_scheme = "Dracula",
-	font_size = 12.0,
-	--	window_decorations = "RESIZE", --Uncomment for window resize buttons
-}
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,26 +22,53 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
+# Misc
+mk() {
+  mkdir --parents "$1" && cd "$1"
+}
+alias rf='rm -rf'
+alias sudo='sudo '  # allow aliases with sudo
+
 # Git
-alias gs='git status'
+alias gi='git init'
+alias gb='git branch'
+alias gba='git branch --all'
+alias gm='git merge'
+alias gn='git checkout -b'  # new branch
+alias gr='git reset'
+alias gu='git pull'
+alias gs='git status --short'
 alias ga='git add'
-alias gc='git commit'
+alias gap='git add --patch'
+alias gc='git commit -m'
+alias gca='git commit --amend --no-edit'
+alias gce='git commit --amend'
+alias gco='git checkout'
+alias gcl='git clone --recursive'
 alias gp='git push'
-alias gl='git log --all --oneline --graph --decorate'
-alias gh='git log --pretty=format:"[%h] %ae, %ar: %s" --stat'
+alias gl='git log --all --oneline --graph --decorate -10'
+alias gh='git log --graph --all --pretty=format:"%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n"'
 alias gd='git difftool'
+alias gds='git difftool --staged'
 
 # tmux
-alias ta='tmux attach -t '
-alias tn='tmux new -s '
-alias tls='tmux list-sessions'
-alias tk='tmux kill-session -t main'
+alias ta='tmux attach -t'
+alias tn='tmux new-session -s'
+alias tl='tmux list-sessions'
+alias tk='tmux kill-session -t'
 
 # Docker
 alias d='docker'
+alias dps='docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"'
 alias dc='docker compose'
 alias dcu='docker compose up'
 alias dcd='docker compose down'
+alias dl='docker logs --tail=100'
+
+# Aliases: systemd
+alias sd='sudo systemctl'
+alias sdu='systemctl --user'
+alias jd='journalctl --no-pager'
 
 # Power (important for iPad-powered Pi)
 alias off='sudo poweroff'
